9dda48995a0241ef8526b32facf675715d082f9f
frank522 <frank.522@osu.edu>
2021-08-13 00:02:46 -0400

    divided Controller component into two divs and added more Control Buttons

--- client/src/Components/Controller.js
+++ client/src/Components/Controller.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 const Controller = () => {
   return (
-    <div>
-      <input type="button" value="reset" id="resetBtn" />
-      <input type="button" value="undo" id="undoBtn" />
-      <input type="button" value="redo" id="redoBtn" />
+    <div id="controller">
+      <div id="editBtns">
+        Edit Buttons
+        <input type="button" value="reset" id="resetBtn" />
+        <input type="button" value="undo" id="undoBtn" />
+        <input type="button" value="redo" id="redoBtn" />
+      </div>
+      <div id="drawBtns">
+        Draw Buttons
+        <input type="button" value="line" id="lineBtn" />
+              <input type="button" value="stroke" id="strokeBtn" />
+              <input type="button" value="rectangle" id="rectangleBtn" />
+              <input type="button" value="square" id="sqaureBtn" />
+              <input type="button" value="circle" id="circleBtn" />              
+      </div>
     </div>
   );
 };
